2a4f83be6f0f47e9cfd9a0f3b45e48bb7e9d04c9
swalxh <williamsalvaradovs1520@hotmail.com>
2020-12-04 07:53:12 -0600

    Se agrego la notacion Lazy a userController

--- src/main/java/mx/uttt/edu/finalev/app/controllers/UserController.java
+++ src/main/java/mx/uttt/edu/finalev/app/controllers/UserController.java
@@ -3,6 +3,7 @@ package mx.uttt.edu.finalev.app.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,6 +24,7 @@ import mx.uttt.edu.finalev.app.models.Usuario;
 public class UserController {
 
 	@Autowired
+	@Lazy
 	IUsuarioDao usuarioDao;
 	
 	@GetMapping({"","/","/login","*"})
